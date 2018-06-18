7ae5a9fbd40952c5b683331175b38f64bc2aed1b
snelakanti <vnelakanti@gmail.com>
2018-06-18 12:58:06 -0400

    import MovieCard to App and test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import './App.css';
+import MovieCard from './components/MovieCard.js'
+// import './App.css';
+
+// const API_KEY = "ad13367e";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-       
+       <MovieCard />
       </div>
     );
   }
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,1 +1,13 @@
-import React from 'react'; 
+import React from 'react';
+
+class MovieCard extends React.Component{
+
+    render(){
+    return(
+<div>
+    Hello
+</div>
+    )}
+}
+
+export default MovieCard;
